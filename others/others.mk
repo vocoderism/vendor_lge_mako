0ce5e6ce48def0d71f1130e71eeb149a2a6653a4
BigBrother1984 <stevewatersy@gmail.com>
2013-11-18 11:43:51 +0100

    Add other missing blobs.
    Signed-off-by: Carlo Savignano <stevewatersy@gmail.com>

--- others/others.mk
+++ others/others.mk
@@ -27,5 +27,12 @@ PRODUCT_COPY_FILES := \
     vendor/lge/mako/others/libpatts_engine_jni_api.so:system/lib/libpatts_engine_jni_api.so \
     vendor/lge/mako/others/libwebrtc_audio_coding.so:system/lib/libwebrtc_audio_coding.so \
     vendor/lge/mako/others/libwebrtc_audio_preprocessing.so:system/lib/libwebrtc_audio_preprocessing.so \
-    vendor/lge/mako/others/libwvdrmengine.so:system/vendor/lib/mediadrm/libwvdrmengine.so
+    vendor/lge/mako/others/libwvdrmengine.so:system/vendor/lib/mediadrm/libwvdrmengine.so \
+    vendor/lge/mako/others/dxhdcp2.b00:system/vendor/firmware/discretix/dxhdcp2.b00 \
+    vendor/lge/mako/others/dxhdcp2.b01:system/vendor/firmware/discretix/dxhdcp2.b01 \
+    vendor/lge/mako/others/dxhdcp2.b02:system/vendor/firmware/discretix/dxhdcp2.b02 \
+    vendor/lge/mako/others/dxhdcp2.b03:system/vendor/firmware/discretix/dxhdcp2.b03 \
+    vendor/lge/mako/others/dxhdcp2.mdt:system/vendor/firmware/discretix/dxhdcp2.mdt \
+    vendor/lge/mako/others/DxHDCP.cfg:system/etc/DxHDCP.cfg \
+    vendor/lge/mako/others/libDxHdcp.so:system/lib/libDxHdcp.so
 
